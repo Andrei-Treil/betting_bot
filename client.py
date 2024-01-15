cf553807bb9069e1b4d4d7a5ef226d733e823cf1
Andrei-Treil <andrei.treil@gmail.com>
2024-01-15 13:46:15 -0500

    init attempt

---
+++ client.py
@@ -0,0 +1,8 @@
+from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper.data import OutputType
+#from basketball_reference_web_scraper import 
+
+client.team_box_scores(day=2,month=12,year=2023)
+client.players_advanced_season_totals()
+print('gooo')
+
