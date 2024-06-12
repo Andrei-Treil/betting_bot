35948d4b7257f3ab8053562ae0a8d1015273a1bf
Andrei-Treil <andrei.treil@gmail.com>
2024-06-11 20:43:53 -0400

    Added concurrent stat stuff (WIP)

--- notebooks/util/selen_scraper.py
+++ notebooks/util/selen_scraper.py
@@ -184,8 +184,151 @@ def get_stats_thread(info):
     driver.quit()
     return info[2],out_dict,missing
 
-# import multiprocessing
+def get_stats_multi_win(info):
+    '''
+    Go through list of dates and populate ALL_TEAMS_CONC
+    dates: list of dates similar to `Tue Oct 29 2013`
+    base_url: base url of NBA stats page for this team
+    abr: team abbreviation
+    RETURNS: dict mapping date -> [cum_stats, last10]
+    '''
+    dates = info[0]
+    base_url = info[1]
+    options = Options()
+    chrome_ops = webdriver.ChromeOptions()
+    chrome_ops.add_argument("--headless=new")
+    options.headless = True
+    driver = webdriver.Chrome(options=options) # requires appropriate webdriver in PATH variables, or pass path to file as arg `executable_path=`
+    out_dict = defaultdict(list)
+    missing = []
 
+    try:
+        for i in range(9,len(dates),10): # open 5 tabs in one run
+            date_list = []
+            k = 0
+            for j in range(i-9,i+1):
+                curr_date = dates[j].split()
+                date_list.append(dates[j])
+                team_stats = base_url + f'&DateTo={MONTH_TO_NUM[curr_date[1]]}%2F{curr_date[2]}%2F{curr_date[3]}&Split=lastn'
+                if j < 82: #TODO: remove this
+                    team_stats += '&SeasonType=Regular+Season'
+                else:
+                    team_stats += '&SeasonType=Playoffs'
+                driver.get(team_stats)
+                k += 1
+                if k < 10:
+                    driver.execute_script("window.open()")
+                    driver.switch_to.window(driver.window_handles[k])
+            
+            window_list = driver.window_handles
+            k = 0
+            # load JS
+            # time.sleep(2)
+            for window in window_list:
+                date = date_list[k]
+                k += 1
+                driver.switch_to.window(window)
+                html = driver.page_source
+
+                soup = BeautifulSoup(html, 'html.parser')
+                results = soup.find_all('table')
+
+                try:
+                    # 24 stats total from i = 1 to 24, only care about 3 - 24
+                    # GP	MIN	PTS	W	L	WIN%	FGM	FGA	FG%	3PM	3PA	3P%	FTM	FTA	FT%	OREB	DREB	REB	AST	TOV	STL	BLK	PF	+/-
+                    cumalitive = results[2].find_all('td') # cum stats to this point
+                    cum_stats = []
+                    for e in cumalitive[3:]: # only care about last ten games?
+                        cum_stats.append(float(e.text))
+
+                    per_n = results[3].find_all('tr') # every 10 games ranges
+                    last_10 = []
+
+                    # if last 10 range includes at least 5 games, use those, else use previous 10 range
+                    if float(per_n[-1].find_all('td')[1].text) >= 5 or i < 10 or len(per_n) < 2:
+                        for e in per_n[-1].find_all('td')[3:]:
+                            last_10.append(float(e.text))
+                        
+                    else:
+                        for e in per_n[-2].find_all('td')[3:]:
+                            last_10.append(float(e.text))
+
+                    out_dict[date].append(cum_stats)
+                    out_dict[date].append(last_10)
+                except:
+                    missing.append(date)
+                if len(driver.window_handles) > 1:
+                    driver.close()
+        
+        date_list = []
+        k = 0
+        remaining = len(dates) % 10
+        if remaining > 0:
+            for i in range(len(dates) - remaining, len(dates)):
+                curr_date = dates[i].split()
+                date_list.append(dates[i])
+                team_stats = base_url + f'&DateTo={MONTH_TO_NUM[curr_date[1]]}%2F{curr_date[2]}%2F{curr_date[3]}&Split=lastn'
+                if i < 82: #TODO: remove this
+                    team_stats += '&SeasonType=Regular+Season'
+                else:
+                    team_stats += '&SeasonType=Playoffs'
+                driver.get(team_stats)
+                k += 1
+                if k < remaining:
+                    driver.execute_script("window.open()")
+                    driver.switch_to.window(driver.window_handles[k])
+
+            
+            window_list = driver.window_handles
+            k = 0
+            # load JS
+            # time.sleep(2)
+            for window in window_list:
+                date = date_list[k]
+                k += 1
+                driver.switch_to.window(window)
+                html = driver.page_source
+
+                soup = BeautifulSoup(html, 'html.parser')
+                results = soup.find_all('table')
+
+                try:
+                    # 24 stats total from i = 1 to 24, only care about 3 - 24
+                    # GP	MIN	PTS	W	L	WIN%	FGM	FGA	FG%	3PM	3PA	3P%	FTM	FTA	FT%	OREB	DREB	REB	AST	TOV	STL	BLK	PF	+/-
+                    cumalitive = results[2].find_all('td') # cum stats to this point
+                    cum_stats = []
+                    for e in cumalitive[3:]: # only care about last ten games?
+                        cum_stats.append(float(e.text))
+
+                    per_n = results[3].find_all('tr') # every 10 games ranges
+                    last_10 = []
+
+                    # if last 10 range includes at least 5 games, use those, else use previous 10 range
+                    if float(per_n[-1].find_all('td')[1].text) >= 5 or i < 10 or len(per_n) < 2:
+                        for e in per_n[-1].find_all('td')[3:]:
+                            last_10.append(float(e.text))
+                        
+                    else:
+                        for e in per_n[-2].find_all('td')[3:]:
+                            last_10.append(float(e.text))
+
+                    out_dict[date].append(cum_stats)
+                    out_dict[date].append(last_10)
+                except:
+                    missing.append(date)
+                if len(driver.window_handles) > 1:
+                    driver.close()
+
+    except Exception as e:
+        driver.quit()
+        print(f'Data for: {info[2]} complete, Captured: {len(out_dict)}, Missing: {len(missing)}, Error: {e}')
+        return info[2],out_dict,missing
+    
+    print(f'Data for: {info[2]} complete, Captured: {len(out_dict)}, Missing: {len(missing)}')
+    driver.quit()
+    return info[2],out_dict,missing
+
+# import multiprocessing
 def one_szn_par(games_df,szn):
     start_date = games_df['date'][0].split()
     teams = []
@@ -197,35 +340,12 @@ def one_szn_par(games_df,szn):
         url = TEAM_ABR_TO_URL[abr] + f'/traditional?Season={szn}&DateFrom={MONTH_TO_NUM[start_date[1]]}%2F{start_date[2]}%2F{start_date[3]}'
         teams.append([dates,url,abr])
         
-    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
-        results = list(executor.map(get_stats_thread,teams))
+    with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+        results = list(executor.map(get_stats_multi_win,teams))
 
     return results
 
 if __name__ == '__main__':
-    #../NBA/games/2023-2024_nba_season.csv
-    games_df = pd.read_csv('NBA/games/2021-2022_season_inj.csv',sep=',',names=['date','away','away_pt','home','home_pt'])
-    ALL_TEAMS_CONC = dict.fromkeys(TEAM_ABR_TO_URL.keys())
-    res = one_szn_par(games_df,'2021-22')
-
-    for r in res:
-        ALL_TEAMS_CONC[r[0]] = r[1]
-        ALL_TEAMS_CONC[r[0]]["MISSED"] = r[2]
-    
-    with open('NBA/conc_stats/2021-2022_conc_stats.pkl', 'wb') as f:
-        pickle.dump(ALL_TEAMS_CONC,f)
-
-    games_df = pd.read_csv('NBA/games/2020-2021_season_inj.csv',sep=',',names=['date','away','away_pt','home','home_pt'])
-    ALL_TEAMS_CONC = dict.fromkeys(TEAM_ABR_TO_URL.keys())
-    res = one_szn_par(games_df,'2020-21')
-
-    for r in res:
-        ALL_TEAMS_CONC[r[0]] = r[1]
-        ALL_TEAMS_CONC[r[0]]["MISSED"] = r[2]
-    
-    with open('NBA/conc_stats/2020-2021_conc_stats.pkl', 'wb') as f:
-        pickle.dump(ALL_TEAMS_CONC,f)
-
     games_df = pd.read_csv('NBA/games/2018-2019_season_inj.csv',sep=',',names=['date','away','away_pt','home','home_pt'])
     ALL_TEAMS_CONC = dict.fromkeys(TEAM_ABR_TO_URL.keys())
     res = one_szn_par(games_df,'2018-19')
@@ -235,4 +355,26 @@ if __name__ == '__main__':
         ALL_TEAMS_CONC[r[0]]["MISSED"] = r[2]
     
     with open('NBA/conc_stats/2018-2019_conc_stats.pkl', 'wb') as f:
+        pickle.dump(ALL_TEAMS_CONC,f)
+
+    games_df = pd.read_csv('NBA/games/2017-2018_season_inj.csv',sep=',',names=['date','away','away_pt','home','home_pt'])
+    ALL_TEAMS_CONC = dict.fromkeys(TEAM_ABR_TO_URL.keys())
+    res = one_szn_par(games_df,'2017-18')
+
+    for r in res:
+        ALL_TEAMS_CONC[r[0]] = r[1]
+        ALL_TEAMS_CONC[r[0]]["MISSED"] = r[2]
+    
+    with open('NBA/conc_stats/2017-2018_conc_stats.pkl', 'wb') as f:
+        pickle.dump(ALL_TEAMS_CONC,f)
+
+    games_df = pd.read_csv('NBA/games/2016-2017_season_inj.csv',sep=',',names=['date','away','away_pt','home','home_pt'])
+    ALL_TEAMS_CONC = dict.fromkeys(TEAM_ABR_TO_URL.keys())
+    res = one_szn_par(games_df,'2016-17')
+
+    for r in res:
+        ALL_TEAMS_CONC[r[0]] = r[1]
+        ALL_TEAMS_CONC[r[0]]["MISSED"] = r[2]
+    
+    with open('NBA/conc_stats/2016-2017_conc_stats.pkl', 'wb') as f:
         pickle.dump(ALL_TEAMS_CONC,f)
